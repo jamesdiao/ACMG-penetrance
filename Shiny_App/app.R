1de1e3243cdf7525b8a572b9585553232b47b054
James Diao <diao.james@gmail.com>
2016-12-08 14:11:39 -0500

    Streamlined code, updated app heatmap/settings

--- Shiny_App/app.R
+++ Shiny_App/app.R
@@ -1,6 +1,6 @@
 #title: "ACMG-ClinVar Penetrance Shiny App"
 #author: "James Diao, under the supervision of Arjun Manrai"
-#date: "November 23, 2016"
+#date: "December 7, 2016"
 
 # Set working directory to file folder
 outdir <- getSrcDirectory(function(dummy) {dummy})
@@ -20,7 +20,7 @@ sapply(pkg_list, require, character.only = T)
 # setwd("/Users/jamesdiao/Documents/Kohane_Lab/2016-paper-ACMG-penetrance/Shiny_App")
 load(file = "disease_level_AF.RData")
 #save(freq_1000g.calc.gene, freq_1000g.count.gene, freq_exac.calc.gene, 
-#     super.levels, file = "disease_level_AF.RData")
+#     freq_gnomad.calc.gene, super.levels, file = "disease_level_AF.RData")
 
 # Read in the given .csv
 DF <- read.csv(file = "../Supplementary_Files/Literature_Prevalence_Estimates.csv", 
@@ -36,7 +36,7 @@ ui <- shinyUI(fluidPage(
       wellPanel(
         h4("Disease-Level Allele Frequencies"),
         radioButtons("dataset", "Select Dataset", 
-                     c("ExAC", "1000 Genomes"))
+                     c("gnomAD","ExAC", "1000G"))
       ),
       wellPanel(
         h4("Penetrance Estimate on Heatmap"),
@@ -45,13 +45,13 @@ ui <- shinyUI(fluidPage(
       wellPanel(
         h4("Allelic Heterogeneity Range"),
         sliderInput("ah_range", "Draws max and min plausible bounds", 
-                    min = 0, max = 1, value = c(0.01,1), step = 0.01)
+                    min = 0, max = 1, value = c(0.01,1), step = 0.05)
       ),
-      wellPanel(
-        h4("Imputed Prevalence Range"),
-        sliderInput("range", "Ex: With range 5, a point prevalence of 0.3 becomes the prevalence range: 0.1-0.5", 
-                    min = 1, max = 30, value = 5, step = 1)
-      ),
+      #wellPanel(
+      #  h4("Imputed Prevalence Range"),
+      #  sliderInput("range", "Ex: With range 5, a point prevalence of 0.3 becomes the prevalence range: 0.1-0.5", 
+      #              min = 1, max = 30, value = 5, step = 1)
+      #),
       wellPanel(
         h4("Generate Plots with Modified Table"),
         actionButton("run", " Make Plots", icon = icon("bar-chart"), styleclass = "success")
@@ -74,10 +74,10 @@ ui <- shinyUI(fluidPage(
       rHandsontableOutput("hot"),
       br(),
       busyIndicator("In Progress: Please Wait", wait = 500),
-      h2("Barplot"),
-      plotOutput("barplot", height = "800px"),
       h2("Heatmap"),
-      plotlyOutput("heatmap", height = "800px")
+      plotlyOutput("heatmap", height = "800px"),
+      h2("Barplot"),
+      plotlyOutput("barplot", height = "800px")
       
     )
   )
@@ -119,9 +119,9 @@ server <- shinyServer(function(input, output, session) {
     # Set Parameters
     finalDF <- isolate(values[["DF"]])
     keep <- finalDF$Evaluate %>% as.logical
-    freq_1000g.calc <- freq_1000g.calc.gene[keep,]
-    freq_1000g.count <- freq_1000g.count.gene[keep,]
-    freq_exac.calc <- freq_exac.calc.gene[keep,]
+    freq_1000g <- freq_1000g.count.gene[keep,]
+    freq_exac <- freq_exac.calc.gene[keep,]
+    freq_gnomad <- freq_gnomad.calc.gene[keep,]
     finalDF <- finalDF[keep,]
     ah_low = input$ah_range[1]
     ah_high = input$ah_range[2]
@@ -130,40 +130,29 @@ server <- shinyServer(function(input, output, session) {
     position = input$position
     pos <- replace(c(F,F,F,F,F), ifelse(position == "Max", 5, 3), T)
     abbrev <- finalDF$Short_Name
-    acmg_ah <- finalDF$Allelic_Heterogeneity %>% strsplit("-") %>% 
-      sapply(as.numeric) %>% sapply(mean) %>% pmax(ah_low) %>% pmin(ah_high)
+    acmg_ah <- finalDF$Allelic_Heterogeneity %>% as.numeric %>% pmax(ah_low) %>% pmin(ah_high)
     
-    sapply(c("Total",super.levels), function(superpop){
+    sapply(c(super.levels,"Total"), function(superpop){
       # Map of disease name to disease tags
-      if (dataset == "1000 Genomes") {
-        find <- "AF_1000G"
-        named.freqs <- freq_1000g.count
-      }
-      if (dataset == "ExAC") {
-        find <- "AF_EXAC"
-        named.freqs <- freq_exac.calc
-      }
+      find <- paste0("AF_", toupper(dataset))
+      named.freqs <- parse(text=paste0("freq_", tolower(dataset))) %>% eval
       if (superpop != "Total") 
         find <- paste(find, superpop, sep = "_")
       named.freqs <- named.freqs[,find] %>% unlist %>% setNames(abbrev)
       allelic.het <- c(ah_low, ah_low, mean(c(ah_low, ah_high)) %>% signif(3), ah_high, ah_high) %>% 
         rep(nrow(finalDF)) %>% matrix(nrow = nrow(finalDF), byrow = T)
       allelic.het[,3] <- acmg_ah
-      sapply(finalDF$Inverse_Prevalence %>% strsplit("-"), function(prev){
-        if (length(prev)>1) {
-          temp <- 1/as.numeric(prev)
-        } else {
-          #take unique prev_1 values and compute a = 2k/(1+r) = lower value of a 5x range
-          temp <- c(range,1)*2/(as.numeric(prev)*(1+range))
-        }
-        return(c(temp[2], temp[2], mean(c(temp[1], temp[2])), temp[1], temp[1]))
-      }) %>% t -> prev_final
       # Matrix of penetrance values for allelic het range, capped at 1
       set_to_na <- function(row) { replace(row, is.infinite(row),NA) %>% pmin(1)}
-      apply(prev_final*allelic.het / named.freqs, 1, set_to_na) %>% unlist
+      apply(allelic.het / finalDF$Inverse_Prevalence / named.freqs, 1, set_to_na) %>% unlist
     }) %>% as.data.frame -> penetrance_init
     
-    order(matrix(penetrance_init$Total, nrow = 5) %>% colSums, decreasing = T) -> ord
+    mat_pen <- matrix(penetrance_init$Total, ncol = 5, byrow = T)
+    if (position == "Max") {
+      ord <- order(mat_pen[,5], mat_pen[,3], mat_pen[,1], decreasing = T)
+    } else {
+      ord <- order(mat_pen[,3], mat_pen[,5], mat_pen[,1], decreasing = T)
+    }
     penetrance_data <- data.frame(penetrance_init, 
                                   "Disease" = factor(sapply(abbrev, 
                                                             function(x) rep(x,5)) %>% as.vector,
@@ -187,28 +176,21 @@ server <- shinyServer(function(input, output, session) {
            ggtitle(sprintf("Penetrance by Ancestry (%s)", dataset)) + 
            theme(axis.text.y=element_text(size=6), 
                  axis.text.x = element_text(angle = -20, hjust = 0.4))
-    #barplot <- ggplotly(barplot, height = 800)
-    # Heatmap
-    #heatmap <- ggplot(aes(x=Disease, y = Subset), data = penetrance_data[pos,]) + coord_flip() + 
-    #       geom_tile(aes(fill = Penetrance), color = 'white') + xlab("Disease") + ylab("Ancestry") +
-    #       scale_fill_gradient(low='white',high = 'darkblue', na.value = "grey50",
-    #                           breaks=c(0,0.25,0.5,0.75,1), labels=c("0","0.25","0.50","0.75","1.00"), limits =c(0,1)) + 
-    #       ggtitle(sprintf("Heatmap: %s Penetrance by Ancestry (%s)", position, dataset)) + 
-    #       theme_minimal() + theme(axis.ticks = element_blank()) + 
-    #       annotate("segment", y=c(0.5,5.5,6.5), yend=c(0.5,5.5,6.5), 
-    #                x=0.5, xend = length(abbrev)+0.5) +
-    #       annotate("segment", y=0.5, yend=6.5, 
-    #                x=c(0.5,length(abbrev)+0.5), 
-    #                xend = c(0.5,length(abbrev)+0.5))
+    barplot <- ggplotly(barplot, height = 800)
     m <- list(l = 170, r = 150, b = -50, t = 100, pad = 5)
+    vals <- unique(scales::rescale(c(penetrance_data$Penetrance))) %>% sort
+    setord <- order(vals, decreasing = FALSE)
+    cols <- scales::col_numeric("Blues", domain = NULL)(vals)
+    colz <- setNames(data.frame(vals[setord], cols[setord]), NULL)
     heatmap <- plot_ly(
-      x = factor(c(super.levels,"Total"), levels = c("Total",super.levels)),
+      x = factor(c(super.levels,"Total"), levels = c(super.levels,"Total")),
       y = factor(sapply(abbrev, function(x) rep(x,5)) %>% as.vector, levels = abbrev[ord]),
-      z = penetrance_init[pos,][ord,] %>% as.matrix %>% signif(3), type = "heatmap", height = 800
+      z = penetrance_init[pos,][ord,] %>% as.matrix %>% signif(3), 
+      type = "heatmap", height = 800, colorscale = colz
     ) %>% layout(autosize = T, margin = m, 
       title = sprintf("%s Penetrance by Ancestry (%s)", position, dataset))
-    output$barplot <- renderPlot({ barplot })
     output$heatmap <- renderPlotly({ heatmap })
+    output$barplot <- renderPlotly({ barplot })
     #cat("Dark gray boxes are NA: no associated variants discovered in that ancestral population.")
   }) #Closes observeEvent "Make Plot"
   
@@ -217,6 +199,19 @@ server <- shinyServer(function(input, output, session) {
 # Run the application 
 shinyApp(ui = ui, server = server)
 
+
+# Heatmap
+#heatmap <- ggplot(aes(x=Disease, y = Subset), data = penetrance_data[pos,]) + coord_flip() + 
+#       geom_tile(aes(fill = Penetrance), color = 'white') + xlab("Disease") + ylab("Ancestry") +
+#       scale_fill_gradient(low='white',high = 'darkblue', na.value = "grey50",
+#                           breaks=c(0,0.25,0.5,0.75,1), labels=c("0","0.25","0.50","0.75","1.00"), limits =c(0,1)) + 
+#       ggtitle(sprintf("Heatmap: %s Penetrance by Ancestry (%s)", position, dataset)) + 
+#       theme_minimal() + theme(axis.ticks = element_blank()) + 
+#       annotate("segment", y=c(0.5,5.5,6.5), yend=c(0.5,5.5,6.5), 
+#                x=0.5, xend = length(abbrev)+0.5) +
+#       annotate("segment", y=0.5, yend=6.5, 
+#                x=c(0.5,length(abbrev)+0.5), 
+#                xend = c(0.5,length(abbrev)+0.5))
 
 #tags$head(tags$script(HTML(JS.logify))),
 #tags$head(tags$script(HTML(JS.onload))),
